5e84b497ebd6fda88802d0efc8026c291868c7e5
hrlnsnchz <hrlnsnchz@gmail.com>
2021-01-13 20:00:27 -0600

    Added tips and locations, fixed typos

---
+++ scripts/tips/Tip.js
@@ -0,0 +1,8 @@
+export const Tips = (tip) => {
+    return `
+    <section class="containerRight__tips">
+                    <ul>
+                        <li>${tip.quote}</li>
+                    </ul>
+    `
+}
---
+++ scripts/tips/TipDataProvider.js
@@ -0,0 +1,21 @@
+const martinsTips = [
+    {
+        quote: "Count and observe the fish"
+    },
+    {
+        quote: "Visual equipment check"
+    },
+    {
+        quote: "Temperature check"
+    },
+    {
+        quote: "Remove uneaten food"
+    },
+    {
+        quote: "Top off the water level"
+    }
+]
+
+export const useTips = () => {
+    return martinsTips.slice()
+}
